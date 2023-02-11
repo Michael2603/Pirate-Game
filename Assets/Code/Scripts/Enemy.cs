12762c3f8f3637edc49c940513ab529dacbf8315
Michael Alcantara <michael.vergil@hotmail.com>
2023-02-11 17:41:37 -0300

    O barco possui um movimento mais suave..

--- Assets/Code/Scripts/Enemy.cs
+++ Assets/Code/Scripts/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inimigo : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     
     protected void Rotacionar()
---
+++ Assets/Code/Scripts/Player.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public float ForwardMovementSpeed;
+    public float RotationSpeed;
+    
+    private Rigidbody2D _rigidbody2d;   
+    private float _inputX;
+    private float _inputY;
+    private float _smoothMovementVelocity;
+    private float _movementInputSmoothVelocity;
+
+
+    private void Start()
+    {
+        _rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    // Takes player input values and converts to ship movement.
+    private void Movement()
+    {
+        // Smooth the ship departure and stop before sending the velocity to Rigidbody.
+        if (_inputY > 0)
+        {
+            _smoothMovementVelocity = Mathf.SmoothDamp(_smoothMovementVelocity, _inputY, ref _movementInputSmoothVelocity, 0.55f);
+        }
+        else
+        {
+            _smoothMovementVelocity = Mathf.SmoothDamp(_smoothMovementVelocity, _inputY, ref _movementInputSmoothVelocity, 1f);
+        }
+
+        _rigidbody2d.velocity = -transform.up * _smoothMovementVelocity * ForwardMovementSpeed;
+
+
+        // Rotates the ship based on its current rotation and the input.
+        if (_inputX != 0)
+        {
+            float angle = _inputX * Mathf.Rad2Deg;
+            transform.localRotation = transform.localRotation * Quaternion.Euler(Vector3.forward * angle * Time.deltaTime * RotationSpeed);
+        }
+    }
+
+    // Takes the vertical and horizontal input and converts to separate variables, better handling movements separately.
+    private void OnMove(InputValue value)
+    {
+        _inputY = value.Get<float>();
+    }
+
+    private void OnRotate(InputValue value)
+    {
+        _inputX = -value.Get<float>();
+    }
+}
