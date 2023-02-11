c2e71e03465472e271533e50a1a34c2b2bde2216
Michael Alcantara <michael.vergil@hotmail.com>
2023-02-11 15:32:36 -0300

    Um mapa template foi criado e foi adicionado o sistema de input.

---
+++ Assets/Codigo/Scripts/Inimigo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    
+    protected void Rotacionar()
+    {
+
+    }
+
+    protected void Avancar()
+    {
+
+    }
+}
