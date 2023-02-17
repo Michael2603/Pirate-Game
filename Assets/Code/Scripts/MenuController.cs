fe122b0eaa77440348a909eebe5193c73135610f
Michael Alcantara <michael.vergil@hotmail.com>
2023-02-17 18:50:36 -0300

    Um menu inicial foi criado e suas interações estão prontas.

---
+++ Assets/Code/Scripts/MenuController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject MainMenuPanel;
+    public GameObject ConfigurationsPanel;
+
+    public Slider GameDurationSlider;
+    public TMP_Text GameDurationValueText;
+
+    public Slider DifficultySlider;
+    public TMP_Text DifficultyValueText;
+
+    //Buttons
+        public void LoadGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void EnterConfigurations()
+        {
+            MainMenuPanel.SetActive(false);
+            ConfigurationsPanel.SetActive(true);
+        }
+
+        public void ReturnFromConfigurations()
+        {
+            ConfigurationsPanel.SetActive(false);
+            MainMenuPanel.SetActive(true);
+        }
+        
+        public void Exit()
+        {
+            Application.Quit();
+        }
+
+    public void AdjustGameDuration()
+    {
+        int durationInMinutes = (int)Mathf.Round(GameDurationSlider.value) / 60;
+
+        GameDurationValueText.text = durationInMinutes + " Minutos";
+    }
+
+    public void AdjustDifficulty()
+    {
+        int difficultyInSecounds = (int)Mathf.Round(DifficultySlider.value);
+
+        DifficultyValueText.text = difficultyInSecounds + " Segundos";
+    }
+}
