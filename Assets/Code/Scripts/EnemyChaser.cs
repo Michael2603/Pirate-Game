9b7e7d4082114ad398da68e5af8db00ef0a9004a
Michael Alcantara <michael.vergil@hotmail.com>
2023-02-20 16:31:44 -0300

    O Chaser também está com a movimentação completa.

--- Assets/Code/Scripts/EnemyChaser.cs
+++ Assets/Code/Scripts/EnemyChaser.cs
@@ -21,31 +21,113 @@ public class EnemyChaser : BoatController
 
     private void Update()
     {
-        // Looks for the player and if found, rotate the boat to face him.
+
+        if (!ChasingEnemy())
+        {
+            WanderAround();
+        }
+
+        // // Looks for the player and if found, rotate the boat to face him.
+        // if (raycast)
+        // {
+        //     Vector3 posRelativeToPlayer = transform.InverseTransformPoint(raycast.transform.position);
+        //     float angleRelativeToPlayer = Mathf.Atan2(posRelativeToPlayer.y, posRelativeToPlayer.x) * Mathf.Rad2Deg;
+            
+        //     if (Mathf.Abs(angleRelativeToPlayer) < 85)
+        //     {
+        //         base.Rotate(1);
+        //         base.MoveForward(0);
+        //     }
+        //     else if (Mathf.Abs(angleRelativeToPlayer) > 95)
+        //     {
+        //         base.Rotate(-1);
+        //         base.MoveForward(0);
+        //     }
+        //     else
+        //     {
+        //         base.Rotate(0);
+        //         base.MoveForward(0.3f);
+        //     }
+        // }
+    }
+
+    // If the enemy is on sight, chase him.
+    private bool ChasingEnemy()
+    {
         RaycastHit2D raycast = Physics2D.CircleCast(transform.position, 5, transform.forward, .1f, 1 << LayerMask.NameToLayer("Player"));
+        
+        // If the enemy is within sight range, only chase him if this boat has direct sight to him.
         if (raycast)
         {
+            RaycastHit2D visionObstructed = Physics2D.Linecast(
+                transform.position,
+                raycast.collider.transform.position,
+                1 << LayerMask.NameToLayer("Island"));
+
+            if (visionObstructed)
+            {
+                return false;
+            }
+
+    
+            // Gets position and angle in relation to the enemy and turns the boat so that it is in attack position in relation to the enemy.
             Vector3 posRelativeToPlayer = transform.InverseTransformPoint(raycast.transform.position);
             float angleRelativeToPlayer = Mathf.Atan2(posRelativeToPlayer.y, posRelativeToPlayer.x) * Mathf.Rad2Deg;
             
             if (Mathf.Abs(angleRelativeToPlayer) < 85)
             {
                 base.Rotate(1);
-                base.MoveForward(0);
+                base.MoveForward(0.1f);
             }
             else if (Mathf.Abs(angleRelativeToPlayer) > 95)
             {
                 base.Rotate(-1);
-                base.MoveForward(0);
+                base.MoveForward(0.1f);
             }
             else
             {
                 base.Rotate(0);
                 base.MoveForward(0.3f);
             }
+
+            return true;
         }
+        else
+        {
+            return false;
+        }
+
     }
 
+    // Wander around the map avoiding colision with islands.
+    private void WanderAround()
+    {
+        RaycastHit2D leftRaycast = Physics2D.Raycast(transform.position, (-transform.up + -transform.right), 4, 1 << LayerMask.NameToLayer("Island"));
+        RaycastHit2D middleRaycast = Physics2D.CircleCast(transform.position + -transform.up * 2.2f, 1.3f,transform.forward, .1f, 1 << LayerMask.NameToLayer("Island"));
+        RaycastHit2D rightRaycast = Physics2D.Raycast(transform.position, (-transform.up + transform.right), 4, 1 << LayerMask.NameToLayer("Island"));
+
+        // Keep straight.
+        if (!middleRaycast)
+        {
+            Rotate(0);
+            MoveForward(.3f);
+            return;
+        }
+        
+        // Rotate to the right.
+        if (leftRaycast)
+        {
+            Rotate(.5f);
+            MoveForward(.2f);
+        }
+        
+        // Rotate to the left.
+        if (rightRaycast)
+        {
+            Rotate(-.5f);
+            MoveForward(.2f);
+        }
+    }
     void OnDrawGizmos()
     {
         // Sight area.
@@ -55,6 +137,11 @@ public class EnemyChaser : BoatController
         // Explosion area.
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, ExplosionRange);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(transform.position, (-transform.up + -transform.right)  * 2.9f);
+        Gizmos.DrawWireSphere(transform.position + -transform.up * 2.2f, 1.3f);
+        Gizmos.DrawRay(transform.position, (-transform.up + transform.right) * 2.9f);
     }
 
     public override void TakeHit(int damage)
